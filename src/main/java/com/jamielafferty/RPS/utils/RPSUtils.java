4fbe825f881645c37bed468d42fa80db24c09026
Jamie Lafferty <jamielafferty99@gmail.com>
2019-06-07 19:52:48 +0200

    Add available moves and function to get a random move

--- src/main/java/com/jamielafferty/RPS/utils/RPSUtils.java
+++ src/main/java/com/jamielafferty/RPS/utils/RPSUtils.java
@@ -1,5 +1,19 @@
 package com.jamielafferty.RPS.utils;
 
-public class RPSUtils {
+import java.util.Random;
 
+public class RPSUtils {
+	
+	// Available moves
+	public static enum Moves {
+		ROCK,
+		PAPER,
+		SCISSORS
+	};
+	
+	// Method to select a random available move
+	public static Moves getRandomMove() {
+		Random random = new Random();
+		return Moves.values()[random.nextInt(Moves.values().length)];
+	}
 }
