bf8e5b16d23e591cc6de7970b04856d190b10251
Jamie Lafferty <jamielafferty99@gmail.com>
2019-06-07 18:33:47 +0200

    Change port which the app will run on

--- src/main/java/com/jamielafferty/RPS/RPSApplication.java
+++ src/main/java/com/jamielafferty/RPS/RPSApplication.java
@@ -3,6 +3,12 @@ package com.jamielafferty.RPS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point will be 9090 (changed in application.yml)
+ * 
+ * @author Jamie
+ *
+ */
 @SpringBootApplication
 public class RPSApplication {
 
